426e02df4e73a3fe1365b9b4fbfc11eb748175ba
Ned Batchelder <ned@nedbatchelder.com>
2024-11-05 08:37:57 -0500

    refactor: these branches are no longer taken

--- coverage/results.py
+++ coverage/results.py
@@ -202,8 +202,7 @@ class Analysis:
         branch_lines = set(self._branch_lines())
         mba = collections.defaultdict(list)
         for l1, l2 in missing:
-            if l1 == l2:
-                continue
+            assert l1 != l2, f"missing_branch_arcs({self.filename!r}): ({l1}, {l2})"
             if l1 in branch_lines:
                 mba[l1].append(l2)
         return mba
@@ -219,8 +218,7 @@ class Analysis:
         branch_lines = set(self._branch_lines())
         eba = collections.defaultdict(list)
         for l1, l2 in self.arcs_executed:
-            if l1 == l2:
-                continue
+            assert l1 != l2, f"executed_branch_arcs({self.filename!r}): ({l1}, {l2})"
             if (l1, l2) not in self.arc_possibilities_set:
                 continue
             if l1 in branch_lines:
